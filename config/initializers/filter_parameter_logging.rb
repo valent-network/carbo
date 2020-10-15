33656138d63e06f915d2602456e008000ce9503d
Viktor Vsk <me@viktorvsk.com>
2020-10-15 21:20:58 +0300

    Log less

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,4 +3,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :access_token]
+Rails.application.config.filter_parameters += [:password, :access_token, :images_json_array_tmp]
