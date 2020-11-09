d1aefe30e42e5be65498502783db0df964b0f8a8
Viktor Vsk <me@viktorvsk.com>
2020-11-09 16:53:01 +0200

    Add ChatRoomUser#name column

---
+++ db/migrate/20201103114215_add_name_to_chat_room_users.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+class AddNameToChatRoomUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column(:chat_room_users, :name, :string, null: false)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2020_11_05_174234) do
     t.bigint("user_id", null: false)
     t.datetime("created_at", precision: 6, null: false)
     t.datetime("updated_at", precision: 6, null: false)
+    t.string("name", null: false)
     t.index(["chat_room_id", "user_id"], name: "index_chat_room_users_on_chat_room_id_and_user_id", unique: true)
     t.index(["chat_room_id"], name: "index_chat_room_users_on_chat_room_id")
     t.index(["user_id"], name: "index_chat_room_users_on_user_id")
