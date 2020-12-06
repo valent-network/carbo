dcf44debdf64375f81c4c6efae4285a2c627acba
Viktor Vsk <me@viktorvsk.com>
2020-12-06 17:33:50 +0200

    Do not initialize S3_CLIENT for CI database setup

--- config/initializers/s3_client.rb
+++ config/initializers/s3_client.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
-Aws.config[:credentials] = Aws::Credentials.new(ENV['DO_SPACE_KEY'], ENV['DO_SPACE_SECRET'])
-S3_CLIENT = Aws::S3::Client.new(region: ENV['DO_SPACE_REGION'], endpoint: "https://#{ENV['DO_SPACE_REGION']}.digitaloceanspaces.com")
+
+if ENV['DO_SPACE_KEY'].present? && ENV['DO_SPACE_SECRET'].present?
+  Aws.config[:credentials] = Aws::Credentials.new(ENV['DO_SPACE_KEY'], ENV['DO_SPACE_SECRET'])
+  S3_CLIENT = Aws::S3::Client.new(region: ENV['DO_SPACE_REGION'], endpoint: "https://#{ENV['DO_SPACE_REGION']}.digitaloceanspaces.com")
+end
