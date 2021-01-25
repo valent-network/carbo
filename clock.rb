ad360e7f139038b916f8e4b05d1d648816f69d2c
Viktor Vsk <me@viktorvsk.com>
2021-01-25 22:00:23 +0200

    Deliver RPush notifications through Clockwork

--- clock.rb
+++ clock.rb
@@ -3,6 +3,10 @@ require './config/boot'
 require './config/environment'
 
 module Clockwork
+  every(1.second, 'RPush: deliver notifications') do
+    Rpush.push
+    Rpush.apns_feedback
+  end
   every(1.day, 'Vacuum Postgresql', at: '05:00', tz: 'UTC') { VacuumDatabase.perform_later }
   every(1.hour, 'Refresh Budget Widget Materialized View') { BudgetWidgetRefreshMaterializedView.perform_later }
   every(1.day, 'Backup Database', at: '03:00', tz: 'UTC') { BackupDatabase.perform_later }
