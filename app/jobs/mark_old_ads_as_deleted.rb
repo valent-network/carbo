3d578fb72720388d4dd8ed5e460fa3ea667eb4bd
Viktor Vsk <me@viktorvsk.com>
2021-02-19 21:31:04 +0200

    Add worker for deleting old ads. And ignore deleted ads for budget widget

---
+++ app/jobs/mark_old_ads_as_deleted.rb
@@ -0,0 +1,7 @@
+class MarkOldAdsAsDeleted < ApplicationJob
+  queue_as(:default)
+
+  def perform
+    Ad.where(deleted: false).where("updated_at <= NOW() - INTERVAL '2 months'").update_all(deleted: true)
+  end
+end
