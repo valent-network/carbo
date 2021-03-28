9940de20191a8b3bce73232e41fcf19c438ae713
Viktor Vsk <me@viktorvsk.com>
2021-03-28 20:59:38 +0300

    Update rubocop config after gem was updated

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,7 +12,7 @@ Rails.application.config.middleware.insert_before(0, Rack::Cors) do
     origins ENV['CORS_ALLOWED_ORIGINS'].to_s.split(',').map(&:strip)
 
     resource '*',
-             headers: :any,
-             methods: %i[get post put patch delete options head]
+      headers: :any,
+      methods: %i[get post put patch delete options head]
   end
 end
