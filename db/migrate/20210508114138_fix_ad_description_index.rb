e15f522dbc354f4e3fb5580a5e9d468392f4b49d
Viktor Vsk <me@viktorvsk.com>
2021-05-08 14:51:05 +0300

    Fix for CI

--- db/migrate/20210508114138_fix_ad_description_index.rb
+++ db/migrate/20210508114138_fix_ad_description_index.rb
@@ -1,6 +1,5 @@
 class FixAdDescriptionIndex < ActiveRecord::Migration[6.1]
   def change
-    remove_index :ad_descriptions, name: :ttt
     remove_index :ad_descriptions, :ad_id
     add_index :ad_descriptions, :ad_id, unique: true
   end
