21b3f50e2cb1f51d41bc8b5616be6d70729e71ac
Viktor Vsk <me@viktorvsk.com>
2021-05-29 02:06:30 +0300

    Slightly more efficient UserContacts upload job in terms of SQL queries

--- app/jobs/upload_user_contacts_job.rb
+++ app/jobs/upload_user_contacts_job.rb
@@ -5,31 +5,51 @@ class UploadUserContactsJob < ApplicationJob
 
   def perform(user_id, contacts)
     user = User.find(user_id)
-    contacts = JSON.parse(contacts)
+    contacts = normalize_contacts!(JSON.parse(contacts))
     initial_contacts_count = user.user_contacts.count
-    User.transaction do
-      contacts.each do |contact|
-        contact['phoneNumbers'].each do |phone|
-          number = Phonelib.parse(phone)
-          next if number.invalid?
+    user_contacts_to_create = []
 
-          phone_number = PhoneNumber.by_full_number(phone).first_or_create
-          next if !phone_number.valid? || !phone_number.persisted?
+    full_phone_numbers = contacts.map { |contact| contact['phoneNumbers'] }.flatten.uniq
+    phone_numbers_ids_hash = process_phone_numbers(full_phone_numbers)
 
-          user_contact = user.user_contacts.where(phone_number: phone_number).first_or_initialize
-          user_contact.name = contact['name'].to_s.strip.gsub(/^(.{97,}?).*$/m, '\1...')
-          user_contact.save
-        end
-      end
-
-      if initial_contacts_count.zero?
-        EffectiveAdsRefreshMaterializedView.perform_now
-        EffectiveUserContactsRefreshMaterializedView.perform_now
-        ApplicationCable::UserChannel.broadcast_to(user, type: 'contacts')
-      else
-        EffectiveAdsRefreshMaterializedView.perform_later
-        EffectiveUserContactsRefreshMaterializedView.perform_later
+    contacts.each do |contact|
+      contact['phoneNumbers'].reject! { |phone| !phone.to_s.in?(phone_numbers_ids_hash.keys) }
+      contact['phoneNumbers'].each do |phone|
+        user_contacts_to_create << {
+          user_id: user_id,
+          name: contact['name'],
+          phone_number_id: phone_numbers_ids_hash[phone.to_s],
+        }
       end
     end
+
+    UserContact.insert_all(user_contacts_to_create) if user_contacts_to_create.present?
+
+    if initial_contacts_count.zero?
+      EffectiveAdsRefreshMaterializedView.perform_now
+      EffectiveUserContactsRefreshMaterializedView.perform_now
+      ApplicationCable::UserChannel.broadcast_to(user, type: 'contacts')
+    else
+      EffectiveAdsRefreshMaterializedView.perform_later
+      EffectiveUserContactsRefreshMaterializedView.perform_later
+    end
+  end
+
+  private
+
+  def normalize_contacts!(contacts)
+    contacts.each do |contact|
+      contact['name'] = contact['name'].to_s.strip.gsub(/^(.{97,}?).*$/m, '\1...')
+      contact['phoneNumbers'].reject! { |phone| Phonelib.parse(phone).invalid_for_country?('UA') }
+      contact['phoneNumbers'].map! { |phone| Phonelib.parse(phone).national.to_s.gsub(/\s/, '').to_i }
+    end
+  end
+
+  def process_phone_numbers(full_phone_numbers)
+    existing_full_phone_numbers = PhoneNumber.where(full_number: full_phone_numbers).pluck(:full_number)
+    full_phone_numbers_to_create = full_phone_numbers - existing_full_phone_numbers
+    PhoneNumber.insert_all(full_phone_numbers_to_create.map { |phone| { full_number: phone } }) if full_phone_numbers_to_create.present?
+
+    Hash[PhoneNumber.where(full_number: full_phone_numbers).pluck(:full_number, :id)]
   end
 end
