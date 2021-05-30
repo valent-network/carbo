7dac36cb078f728aff1bc80058dbfaa1d5934d94
Viktor Vsk <me@viktorvsk.com>
2021-05-30 15:03:24 +0300

    Rubocop -A

--- config/initializers/turbo_sms_fix_unauthorized.rb
+++ config/initializers/turbo_sms_fix_unauthorized.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # https://gist.github.com/yanaizmene/d721a7d8393d9c6a6c74f06063ab48dd
 # https://github.com/vitalikdanchenko/turbosms/issues/4
 
@@ -5,11 +6,11 @@ module TurboSMS
   class << self
     def send_sms(destination, text = nil, sender = nil)
       authorize
-      
+
       message = {
-        sender:       sender || default_options[:sender],
-        destination:  destination,
-        text:         text
+        sender: sender || default_options[:sender],
+        destination: destination,
+        text: text,
       }
       result = authorised_call(:send_sms, message: message)
       raise SendingSMSError, result unless result.instance_of?(Array)
