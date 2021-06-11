f7f87e713a376c7beef544e95ded6bf8ec4841d3
Viktor Vsk <me@viktorvsk.com>
2021-06-11 17:26:57 +0300

    Fix year and price filtering

--- app/queries/effective_ads.rb
+++ app/queries/effective_ads.rb
@@ -4,10 +4,10 @@ class EffectiveAds
     ads = Ad.from('effective_ads AS ads').group('ads.id, ads.phone_number_id')
     ads = ads.select('DISTINCT ON (ads.id) ads.id, ads.phone_number_id')
 
-    ads = ads.where('price >= ?', filters[:min_price]) if filters[:min_price].present?
-    ads = ads.where('price <= ?', filters[:max_price]) if filters[:max_price].present?
-    ads = ads.where("year >= ?", filters[:min_year]) if filters[:min_year].present?
-    ads = ads.where("year <= ?", filters[:max_year]) if filters[:max_year].present?
+    ads = ads.where('price >= ?', filters[:min_price].to_i) if filters[:min_price].present?
+    ads = ads.where('price <= ?', filters[:max_price].to_i) if filters[:max_price].present?
+    ads = ads.where("year >= ?", filters[:min_year].to_i) if filters[:min_year].present?
+    ads = ads.where("year <= ?", filters[:max_year].to_i) if filters[:max_year].present?
 
     ads = ads.where("fuel IN (?)", filters[:fuels]) if filters[:fuels].present?
     ads = ads.where("gear IN (?)", filters[:gears]) if filters[:gears].present?
