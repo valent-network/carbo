8f2f19d6ca0e7c2e0f65878c3b4e20351fb259de
Viktor Vsk <me@viktorvsk.com>
2021-06-12 16:33:45 +0300

    Add HTML page with very basic marketing text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,10 @@ class ApplicationController < ActionController::Base
     @meta_title = JSON.parse(@page.meta)['title']
   end
 
+  def network
+    render('/network', layout: false)
+  end
+
   protected
 
   attr_reader :current_user, :current_device, :current_ads_source
