0c3f8de3e71ac77be9cf9ee66a1b1568cf37e2df
Viktor Vsk <me@viktorvsk.com>
2021-06-15 14:51:05 +0300

    Improve network UI

--- app/controllers/promo_events_controller.rb
+++ app/controllers/promo_events_controller.rb
@@ -3,7 +3,7 @@ class PromoEventsController < ApplicationController
   layout 'network'
 
   def index
-    @events = PromoEvent.page(params[:page])
+    @events = PromoEvent.order(id: :desc).page(params[:page]).per(8)
     @events = @events.where(refcode: params[:refcode].strip.upcase) if params[:refcode].to_s.strip.size == 5
   end
 end
