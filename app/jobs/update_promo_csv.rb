558c0762e97f0fb6967d8c53ee11b6a6e1b17eda
Viktor Vsk <me@viktorvsk.com>
2021-06-23 16:42:57 +0300

    Improve promo csv content

--- app/jobs/update_promo_csv.rb
+++ app/jobs/update_promo_csv.rb
@@ -6,7 +6,18 @@ class UpdatePromoCsv < ApplicationJob
   queue_as :default
 
   def perform
-    %x(psql -h $POSTGRESQL_SERVICE_HOST -U $POSTGRES_USER -d $POSTGRES_DATABASE -c '\\copy (SELECT id, full_phone_number_masked, name, created_at FROM promo_events_matview) TO '#{PROMO_CSV_LOCAL_TEMP_LOCATION}' WITH csv')
+    promo_events_sql = <<~SQL
+      SELECT id,
+             full_phone_number_masked,
+             CASE WHEN name = 'sign_up' THEN 'Установка приложения'
+                  WHEN name = 'set_referrer' THEN 'Указан пригласивший пользователь'
+                  WHEN name = 'invited_user' THEN 'Приглашён пользователь'
+             END AS action,
+             to_char(created_at AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Kiev', 'DD-MM-YYYY, HH24:MI') AS datetime
+      FROM promo_events_matview
+      ORDER BY id DESC
+    SQL
+    %x(psql -h $POSTGRESQL_SERVICE_HOST -U $POSTGRES_USER -d $POSTGRES_DATABASE -c "\\copy (#{promo_events_sql}) TO '#{PROMO_CSV_LOCAL_TEMP_LOCATION}' WITH csv")
 
     S3_CLIENT.put_object(bucket: ENV['DO_SPACE_NAME'], key: PROMO_CSV_DO_SPACE_LOCATION, body: File.new(PROMO_CSV_LOCAL_TEMP_LOCATION), acl: 'public-read')
   end
