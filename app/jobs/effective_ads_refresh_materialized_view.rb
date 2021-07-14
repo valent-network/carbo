ce7f91efaad1edfe38f27de4968a5b29699a0034
Viktor Vsk <me@viktorvsk.com>
2021-07-14 11:31:26 +0300

    Temp

--- app/jobs/effective_ads_refresh_materialized_view.rb
+++ app/jobs/effective_ads_refresh_materialized_view.rb
@@ -4,6 +4,7 @@ class EffectiveAdsRefreshMaterializedView < ApplicationJob
   queue_as(:default)
 
   def perform
-    %x(psql -h $POSTGRESQL_SERVICE_HOST -U $POSTGRES_USER -d $POSTGRES_DATABASE -c "REFRESH MATERIALIZED VIEW CONCURRENTLY effective_ads")
+    # TODO: TEMP
+    # %x(psql -h $POSTGRESQL_SERVICE_HOST -U $POSTGRES_USER -d $POSTGRES_DATABASE -c "REFRESH MATERIALIZED VIEW CONCURRENTLY effective_ads")
   end
 end
