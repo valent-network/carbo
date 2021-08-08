80ec66a6d1300e43492caf95fa27e74f8e4f97f9
Viktor Vsk <me@viktorvsk.com>
2021-08-08 12:07:17 +0300

    Initially uploaded contacts dont block sidekiq workers idling and waiting for matviews refreshed. Instead, check this async

---
+++ app/workers/initial_contacts_upload.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+class InitialContactsUpload
+  include Sidekiq::Worker
+
+  sidekiq_options queue: 'default', retry: true, backtrace: false
+
+  def perform(user_id, init_time)
+    init_time = Time.at(init_time)
+    ads_last_refreshed_at = REDIS.get('server.effective_ads.last_refreshed_at')
+    contacts_last_refreshed_at = REDIS.get('server.effective_user_contacts.last_refreshed_at')
+
+    if ads_last_refreshed_at.present? && contacts_last_refreshed_at.present? && Time.zone.parse(ads_last_refreshed_at).after?(init_time) && Time.zone.parse(contacts_last_refreshed_at).after?(init_time)
+      user = User.find(user_id)
+      ApplicationCable::UserChannel.broadcast_to(user, type: 'contacts')
+    else
+      self.class.perform_in(5.seconds, user_id, init_time.to_i)
+    end
+  end
+end
--- app/workers/upload_user_contacts_job.rb
+++ app/workers/upload_user_contacts_job.rb
@@ -12,7 +12,7 @@ class UploadUserContactsJob
     user_contacts_to_upsert = []
 
     full_phone_numbers = contacts.map { |contact| contact['phoneNumbers'] }.flatten.uniq
-    return if full_phone_numbers.blank?
+    ApplicationCable::UserChannel.broadcast_to(user, type: 'contacts') && return if full_phone_numbers.blank?
 
     PhoneNumber.insert_all(full_phone_numbers.map { |phone| { full_number: phone } }, unique_by: [:full_number])
 
@@ -34,10 +34,7 @@ class UploadUserContactsJob
     UserContact.upsert_all(user_contacts_to_upsert, unique_by: [:phone_number_id, :user_id]) if user_contacts_to_upsert.present?
     CreateEvent.call(:uploaded_contatcts, user: user, data: { contacts_count: full_phone_numbers.count })
 
-    if initial_contacts_count.zero?
-      wait_for_refresh
-      ApplicationCable::UserChannel.broadcast_to(user, type: 'contacts')
-    end
+    InitialContactsUpload.perform_in(5.seconds, user.id, Time.zone.now.to_i) if initial_contacts_count.zero?
   end
 
   private
@@ -50,22 +47,6 @@ class UploadUserContactsJob
         parsed.valid_for_country?(:UA) && parsed.types.include?(:mobile)
       end
       contact['phoneNumbers'].map! { |phone| Phonelib.parse(phone).national.to_s.gsub(/\s/, '').to_i.to_s }
-    end
-  end
-
-  def wait_for_refresh
-    iterations = 0
-    init_time = Time.zone.now
-    loop do
-      ads_last_refreshed_at = REDIS.get('server.effective_ads.last_refreshed_at')
-      contacts_last_refreshed_at = REDIS.get('server.effective_user_contacts.last_refreshed_at')
-
-      break if ads_last_refreshed_at.present? && contacts_last_refreshed_at.present? && Time.zone.parse(ads_last_refreshed_at).after?(init_time) && Time.zone.parse(contacts_last_refreshed_at).after?(init_time)
-
-      iterations += 1
-      break if iterations == 10
-      Rails.logger.warn("[UploadUserContactsJob][InitialContactsZero][Waiting][#{iterations}]")
-      sleep(1)
     end
   end
 end
