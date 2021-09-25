8c95ad7a7a82f5873416f634622f4bc4cdd08988
Viktor Vsk <me@viktorvsk.com>
2021-09-25 10:35:20 +0300

    Snapshot user visibitiy by chunks

--- app/workers/snapshot_user_visibility.rb
+++ app/workers/snapshot_user_visibility.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class SnapshotUserVisibility
+  BATCH_SIZE = 100
+
   include Sidekiq::Worker
 
   sidekiq_options queue: 'default', retry: true, backtrace: false
 
   def perform
-    User.find_each do |user|
+    users = User.joins("LEFT JOIN events ON events.user_id = users.id AND events.name = 'snapshot_user_visibility' AND events.created_at > (NOW() - INTERVAL '1 day')")
+      .where('events.id IS NULL')
+      .limit(BATCH_SIZE)
+
+    users.each do |user|
       CreateEvent.call(:snapshot_user_visibility, user: user, data: user.current_visibility)
     end
   end
