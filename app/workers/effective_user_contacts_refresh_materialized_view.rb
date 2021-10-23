1e86e6d63fdaeca384102c7a712a8b1046397c09
Viktor Vsk <me@viktorvsk.com>
2021-10-23 16:20:43 +0300

    Temporarily disable refresh effective_user_contacts matview

--- app/workers/effective_user_contacts_refresh_materialized_view.rb
+++ app/workers/effective_user_contacts_refresh_materialized_view.rb
@@ -9,7 +9,7 @@ class EffectiveUserContactsRefreshMaterializedView
     last_refreshed_at = REDIS.get('server.effective_user_contacts.last_refreshed_at')
     return if last_refreshed_at.present? && (Time.zone.now - Time.zone.parse(last_refreshed_at)) < 5
 
-    %x(psql -h $POSTGRESQL_SERVICE_HOST -U $POSTGRES_USER -d $POSTGRES_DATABASE -c "REFRESH MATERIALIZED VIEW CONCURRENTLY effective_user_contacts")
+    # %x(psql -h $POSTGRESQL_SERVICE_HOST -U $POSTGRES_USER -d $POSTGRES_DATABASE -c "REFRESH MATERIALIZED VIEW CONCURRENTLY effective_user_contacts")
     REDIS.set('server.effective_user_contacts.last_refreshed_at', Time.zone.now)
   end
 end
