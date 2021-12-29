0b07b58f2ee8e6a48a7fb09c2ca983093bdae1f2
Viktor Vsk <me@viktorvsk.com>
2021-12-29 14:04:35 +0200

    Remove unused

--- app/queries/known_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-class KnownUsers
-  def call(user_id)
-    <<~SQL
-      SELECT connection_id AS id FROM user_connections WHERE user_id = #{user_id}
-    SQL
-  end
-end
