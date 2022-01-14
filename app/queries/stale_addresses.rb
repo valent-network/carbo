5237b06fdb9ad94376394106e5fb0c8a6b5b097f
Viktor Vsk <me@viktorvsk.com>
2022-01-14 23:34:07 +0200

    Fix StaleAddresses and make configurable

--- app/queries/stale_addresses.rb
+++ app/queries/stale_addresses.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 class StaleAddresses
-  ADDRESSES_BATCH_LIMIT = 100
+  BATCH_LIMIT = ENV.fetch('STALE_ADDRESSES_BATCH_LIMIT', 100)
+  UPDATE_INTERVAL = ENV.fetch('STALE_ADDRESSES_UPDATE_UINTERVAL_HOURS', 24).hours.to_i
 
   def self.call
+    update_interval_ago = Time.zone.now - UPDATE_INTERVAL
+
     Ad.select(:address)
       .joins('JOIN effective_ads ON effective_ads.id = ads.id')
-      .limit(ADDRESSES_BATCH_LIMIT)
+      .where('ads.updated_at < ?', update_interval_ago)
+      .limit(BATCH_LIMIT)
   end
 end
