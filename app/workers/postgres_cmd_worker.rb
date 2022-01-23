86d4438e0fa4c62e3aa9f9ce54070611af8d1b22
Viktor Vsk <me@viktorvsk.com>
2022-01-24 00:06:41 +0200

    Revert refression for effective_ads matview

--- app/workers/postgres_cmd_worker.rb
+++ app/workers/postgres_cmd_worker.rb
@@ -5,6 +5,12 @@ class PostgresCmdWorker
   sidekiq_options queue: 'refresh-matviews', retry: true, backtrace: false
 
   def perform(table_name)
+
+    if effective_ads
+      last_refreshed_at = REDIS.get('server.effective_ads.last_refreshed_at')
+      return if last_refreshed_at.present? && (Time.zone.now - Time.zone.parse(last_refreshed_at)) < 5
+    end
+
     cmd = [
       'psql',
       '-h',
@@ -14,9 +20,11 @@ class PostgresCmdWorker
       '-d',
       ENV['POSTGRES_DATABASE'],
       '-c',
-      "\"REFRESH MATERIALIZED VIEW #{table_name}\""
+      "\"REFRESH MATERIALIZED VIEW CONCURRENTLY #{table_name}\""
     ].join(' ')
 
     %x(#{cmd})
+
+    REDIS.set('server.effective_ads.last_refreshed_at', Time.zone.now) if table_name == 'effective_ads'
   end
 end
