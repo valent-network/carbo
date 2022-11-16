f9b0f371740bd3ce4ab05f7fac3602b93192f6a4
Viktor Vsk <me@viktorvsk.com>
2022-11-16 15:53:08 +0100

    Control RAILS_LOG_LEVEL with ENV - fux after enforcing Rails 7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :info
+  config.log_level = ENV["RAILS_LOG_LEVEL"].to_sym
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
