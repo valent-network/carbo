e119047d89b4b69ffe46309c759f960377177892
Viktor Vsk <me@viktorvsk.com>
2022-12-08 00:10:29 +0100

    AdOptions and FilterableValues are depndent: :destroy when AdOptionType is destroyed

--- app/models/ad_option_type.rb
+++ app/models/ad_option_type.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class AdOptionType < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  has_many :ad_options
+  has_many :ad_options, dependent: :destroy
   has_many :ad_option_values, through: :ad_options
+  has_many :filterable_values, dependent: :destroy
 end
