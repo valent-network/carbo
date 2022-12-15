fbeab40fc524e1f3e2f2c488ff20be8fcb1f81fb
Viktor Vsk <me@viktorvsk.com>
2022-12-15 18:48:33 +0100

    Refactor TMP migration worker

--- app/workers/tmp_migrate_short_description.rb
+++ app/workers/tmp_migrate_short_description.rb
@@ -6,22 +6,23 @@ class TmpMigrateShortDescription
   sidekiq_options queue: 'system', retry: true, backtrace: false
 
   def perform
-    ads = Ad.joins(:ad_extra)
+    Ad.joins(:ad_extra)
       .left_joins(:ad_description)
       .includes(:ad_extra)
       .select('ads.id, ad_extras.details AS ad_extra_details')
       .distinct('ads.id')
       .where(ad_descriptions: { short: [nil, ''] })
-      .limit(10_000)
+      .find_in_batches(batch_size: 10000) do |ads|
+        to_upsert = []
 
-    to_upsert = ads.filter_map do |ad|
-      short = AdCarShortDescriptionPresenter.new.call(ad.ad_extra_details)
-      next if short.blank?
+        ads.each do |ad|
+          short = AdCarShortDescriptionPresenter.new.call(ad.ad_extra_details)
+          to_upsert << { ad_id: ad.id, short: short } if short.present?
+        end
 
-      { ad_id: ad.id, short: short }
-    end
+        puts to_upsert.size
 
-    res = AdDescription.upsert_all(to_upsert, unique_by: [:ad_id]) if to_upsert.present?
-    res&.count
+        AdDescription.upsert_all(to_upsert, unique_by: [:ad_id]) if to_upsert.present?
+      end
   end
 end
