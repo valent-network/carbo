3836ef0c695d4f7e1d8ec27aedef787c4a0c52ac
Viktor Vsk <me@viktorvsk.com>
2023-01-03 12:58:08 +0100

    Add FVG model

---
+++ app/models/filterable_values_group.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+class FilterableValuesGroup < ApplicationRecord
+  belongs_to :ad_option_type
+  validates :name, presence: true, uniqueness: { scope: [:ad_option_type_id] }
+  has_many :values, primary_key: :name, foreign_key: :name, class_name: 'FilterableValue'
+end
