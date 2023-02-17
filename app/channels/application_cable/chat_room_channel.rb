a4a1f557d1c6258774efc4c0d42d33f131cdc5ec
Viktor Vsk <me@viktorvsk.com>
2023-02-17 22:08:15 +0100

    Handle ws requests from chat when admin/user screen was used

--- app/channels/application_cable/chat_room_channel.rb
+++ app/channels/application_cable/chat_room_channel.rb
@@ -5,15 +5,17 @@ module ApplicationCable
       @chat_room = ChatRoom.eager_load(chat_room_users: [user: :phone_number], ad: [:ad_description, :ad_image_links_set, :city, :ad_query, :ad_extra], messages: [:user, :chat_room]).find(params[:chat_room_id])
       @chat_room_user = @chat_room.chat_room_users.find_by(user: current_user)
 
-      # TODO: user can't receive this from UserChannel if ChatRoomChannel
-      # subscription fired before UserChannel subscription
-      @chat_room_user&.touch
-      payload = Api::V1::ChatRoomListSerializer.new(current_user, @chat_room).first
-      ApplicationCable::UserChannel.broadcast_to(current_user, type: 'read_update', chat: payload)
-
-      if @chat_room.system? && current_user.admin?
+      if params[:from] == 'admin' && current_user.admin? && @chat_room.system?
         @chat_room.update(admin_seen_at: Time.zone.now)
+        payload = Api::V1::ChatRoomListSerializer.new(current_user, @chat_room).first
+        ApplicationCable::UserChannel.broadcast_to(current_user, type: 'admin_read_update', chat: payload)
         ApplicationCable::UserChannel.broadcast_to(current_user, type: 'unread_update', count: Message.unread_messages_for(current_user.id).count, system_count: Message.unread_system_messages.values.sum)
+      else
+        # TODO: user can't receive this from UserChannel if ChatRoomChannel
+        # subscription fired before UserChannel subscription
+        @chat_room_user.touch
+        payload = Api::V1::ChatRoomListSerializer.new(current_user, @chat_room).first
+        ApplicationCable::UserChannel.broadcast_to(current_user, type: 'read_update', chat: payload)
       end
 
       stream_for(@chat_room)
@@ -23,6 +25,11 @@ module ApplicationCable
       @chat_room_user&.touch
       payload = Api::V1::ChatRoomListSerializer.new(current_user, @chat_room).first
       ApplicationCable::UserChannel.broadcast_to(current_user, type: 'read_update', chat: payload)
+    end
+
+    def admin_read(_data)
+      payload = Api::V1::ChatRoomListSerializer.new(current_user, @chat_room).first
+      ApplicationCable::UserChannel.broadcast_to(current_user, type: 'admin_read_update', chat: payload)
       if @chat_room.system? && current_user.admin?
         @chat_room.update(admin_seen_at: Time.zone.now)
         ApplicationCable::UserChannel.broadcast_to(current_user, type: 'unread_update', count: Message.unread_messages_for(current_user.id).count, system_count: Message.unread_system_messages.values.sum)
@@ -44,12 +51,8 @@ module ApplicationCable
       end
     end
 
-    # TODO: Here we don't handle a case when admin sends message to a system chat
-    # (to himself)
     def receive(data)
-      raise if !current_user.admin? && @chat_room.system? && @chat_room_user.nil?
-
-      if current_user.admin? && @chat_room.system? && @chat_room_user.nil?
+      if current_user.admin? && @chat_room.system? && data[:from] == 'admin'
         chat_room = ChatRoom.find(data['message']['chat_room_id'])
         SendSystemMessageToChatRoom.new.call(user_id: chat_room.user_id, message_text: data['message']['text'], message_id: data['message']['_id'])
         payload = Api::V1::ChatRoomListSerializer.new(current_user, chat_room).first
