f28c1b2b3c655f5ead0197b3af13407155c0b4c3
Viktor Vsk <me@viktorvsk.com>
2023-02-27 16:47:24 +0100

    Debug

--- clock.rb
+++ clock.rb
@@ -21,6 +21,8 @@ module Clockwork
 
   every(1.day, 'Snapshot Dashboard Stats', skip_first_run: true, tz: 'UTC') { enqueue('class' => 'SnapshotSystemStats') }
 
+  every(6.hours, 'Update User statistics', skip_first_run: true, tz: 'UTC') { enqueue('class' => 'UpdateUserStats') }
+
   every(1.hour, 'Delete Old VerificationRequests', skip_first_run: true, tz: 'UTC') { enqueue('class' => 'DeleteOldVerificationRequests') }
 
   every(1.hour, 'Update Ad statistics', skip_first_run: true, tz: 'UTC') { enqueue('class' => 'UpdateAdStats') }
@@ -28,8 +30,6 @@ module Clockwork
   every(1.hour, 'Delete Old Presigned Images', skip_first_run: true, tz: 'UTC') { enqueue('class' => 'CleanupTempAdImage') }
 
   every(1.hour, 'Provider.crawl', skip_first_run: true, tz: 'UTC') { enqueue('class' => 'AutoRia::IndexCrawler', 'queue' => 'provider', 'lock' => 'until_expired', 'lock_ttl' => 3600) }
-
-  every(5.minutes, 'Update User statistics', skip_first_run: true, tz: 'UTC') { enqueue('class' => 'UpdateUserStats') }
 
   every(1.minute, 'Request Provider to Actualize Ads', skip_first_run: true, tz: 'UTC') { enqueue('class' => 'ActualizeAd', 'queue' => 'ads', 'lock' => 'until_expired', 'lock_ttl' => 3600) }
 end
