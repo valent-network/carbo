424a42db9fac42d4ada67867051ee19cab644d10
Viktor Vsk <me@viktorvsk.com>
2023-04-25 15:03:10 +0200

    Use Redis Sentinel for regular Redis (RedisGraph WIP)

--- config/initializers/_redis.rb
+++ config/initializers/_redis.rb
@@ -1,9 +1,23 @@
 # frozen_string_literal: true
-REDIS = Redis.new(
-  host: ENV.fetch('REDIS_SERVICE_HOST', 'localhost'),
-  password: ENV.fetch('REDIS_SERVICE_PASSWORD', nil),
-  port: ENV.fetch('REDIS_SERVICE_PORT', '6379'),
-)
+
+redis_configuration = if ENV['REDIS_SENTINEL_ENABLED'].present?
+  sentinels = ENV['REDIS_SENTINELS'].to_s.split(',').map(&:strip).map { |s| s.split(':') }.map { |s| { host: s.first, port: s.last, password: ENV['REDIS_PASSWORD'] } }
+  {
+    name: ENV['REDIS_MASTER_SET'],
+    sentinels: sentinels,
+    role: :master,
+    host: ENV['REDIS_MASTER_SET'],
+    password: ENV['REDIS_PASSWORD'],
+  }
+else
+  {
+    host: ENV.fetch('REDIS_SERVICE_HOST', 'localhost'),
+    password: ENV.fetch('REDIS_SERVICE_PASSWORD', nil),
+    port: ENV.fetch('REDIS_SERVICE_PORT', '6379'),
+  }
+end
+
+REDIS = Redis.new(redis_configuration)
 
 REDIS_KEYS = [
   'provider.proxy.pool'
