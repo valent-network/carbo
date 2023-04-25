4568995df6105e25a0e2bde2a9079d2c047f4c91
Viktor Vsk <me@viktorvsk.com>
2023-04-25 16:10:32 +0200

    Sidekiq fix redis URL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = REDIS_CONFIGURATION.except(:host).merge(url: REDIS_CONFIGURATION[:host])
+  config.redis = REDIS_CONFIGURATION.except(:host).merge(url: "redis://#{REDIS_CONFIGURATION[:host]}")
   config.logger.level = defined?(Rails) ? Rails.logger.level : :warn
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = REDIS_CONFIGURATION.except(:host).merge(url: REDIS_CONFIGURATION[:host])
+  config.redis = REDIS_CONFIGURATION.except(:host).merge(url: "redis://#{REDIS_CONFIGURATION[:host]}")
   config.logger.level = defined?(Rails) ? Rails.logger.level : :warn
 end
