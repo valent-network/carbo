4bcd1a135219a4c09d388b9508ecdf2e145d8aac
Viktor Vsk <me@viktorvsk.com>
2023-05-08 15:08:39 +0200

    Do not specify logger in Sidekiq since we use Sentry explicitly

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,12 +2,8 @@
 
 Sidekiq.configure_server do |config|
   config.redis = REDIS_CONFIGURATION.except(:host).merge(url: "redis://#{REDIS_CONFIGURATION[:host]}")
-  config.logger.level = defined?(Rails) ? Rails.logger.level : :warn
-  config.logger = Rails.logger if defined?(Rails)
 end
 
 Sidekiq.configure_client do |config|
   config.redis = REDIS_CONFIGURATION.except(:host).merge(url: "redis://#{REDIS_CONFIGURATION[:host]}")
-  config.logger.level = defined?(Rails) ? Rails.logger.level : :warn
-  config.logger = Rails.logger if defined?(Rails)
 end
