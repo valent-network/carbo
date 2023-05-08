8a30d0e9a7eeb0a11c77e4cd780d92ac40d2f2b1
Viktor Vsk <me@viktorvsk.com>
2023-05-08 16:03:24 +0200

    Disable standard cop for Lint/ConstantDefinitionInBlock in Rails constants initializers

--- config/initializers/amplitude_client.rb
+++ config/initializers/amplitude_client.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.configuration.after_initialize do
-  AMPLITUDE_CLIENT = AmplitudeClient.new(ENV["AMPLITUDE_API_KEY"])
+  AMPLITUDE_CLIENT = AmplitudeClient.new(ENV["AMPLITUDE_API_KEY"]) # standard:disable Lint/ConstantDefinitionInBlock
 end
--- config/initializers/user_friends_graph.rb
+++ config/initializers/user_friends_graph.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.reloader.to_prepare do
-  USER_FRIENDS_GRAPH = UserFriendsGraph.new
+  USER_FRIENDS_GRAPH = UserFriendsGraph.new # standard:disable Lint/ConstantDefinitionInBlock
   USER_FRIENDS_GRAPH.index_users
 end
