56c1373fa809d82b02cd2492a257d0cc17e801fd
Viktor Vsk <me@viktorvsk.com>
2023-05-09 18:30:31 +0200

    Avoid using ADMIN_USERS ENV variable because User#admin already exists

--- app/actions/send_system_message_to_admins.rb
+++ app/actions/send_system_message_to_admins.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 class SendSystemMessageToAdmins
-  ADMIN_USERS_IDS = ENV["ADMIN_USERS_IDS"].to_s.split
-
   def call(message_text)
     sender = SendSystemMessageToChatRoom.new
 
-    ADMIN_USERS_IDS.each do |user_id|
-      user = User.find(user_id)
+    User.where(admin: true).each do |user|
       sender.call(user_id: user.id, message_text: message_text)
     end
   end
--- spec/actions/send_system_message_to_admins_spec.rb
+++ spec/actions/send_system_message_to_admins_spec.rb
@@ -3,10 +3,9 @@
 require "rails_helper"
 
 RSpec.describe(SendSystemMessageToAdmins) do
-  let(:user) { create(:user) }
+  let(:user) { create(:user, admin: true) }
 
   it "sends message to admin user set via ENV variable" do
-    stub_const("SendSystemMessageToAdmins::ADMIN_USERS_IDS", [user.id])
     expect_any_instance_of(SendSystemMessageToChatRoom).to(receive(:call).with(user_id: user.id, message_text: "arbitary text"))
     subject.call("arbitary text")
   end
