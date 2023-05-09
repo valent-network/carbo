bf37767f3897ab1d5dc69030760d0c37e078c983
Viktor Vsk <me@viktorvsk.com>
2023-05-09 13:01:26 +0200

    Safe invokation of CachedSettings in initializers

--- config/initializers/cached_settings.rb
+++ config/initializers/cached_settings.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.configuration.after_initialize do
-  CachedSettings.refresh
+  CachedSettings.refresh if Category.table_exists?
+rescue ActiveRecord::NoDatabaseError
+  Rails.logger.warn("Skip loading cached settings since database connection was not established. Most likely, this run was initiated by `rails db:migrate` command")
 end
