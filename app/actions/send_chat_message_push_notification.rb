7b1c7412b5ce1fb6c066e4700f941efcb1e6d6f8
Viktor Vsk <me@viktorvsk.com>
2023-05-09 12:50:38 +0200

    Fix bug with sending chat push notification app selection for android + adjust for fresh install

--- app/actions/send_chat_message_push_notification.rb
+++ app/actions/send_chat_message_push_notification.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class SendChatMessagePushNotification
-  APPS = {
-    "ios" => Rpush::Client::ActiveRecord::Apnsp8::App.find_by_name("ios"),
-    "android" => Rpush::Client::ActiveRecord::Gcm::App.find_by_name("android")
-  }
-
   def call(message:, chat_room_user:)
     message_body = message.user ? "#{message.chat_room.chat_room_users.find_by(user: message.user).name}: #{message.body}" : message.body
     ad = chat_room_user.chat_room.ad
@@ -13,13 +8,12 @@ class SendChatMessagePushNotification
     user_devices_to_receive_notification = chat_room_user.user.user_devices.where.not(push_token: ["", nil]).where(os: %w[ios android])
     ad_title = ad ? ad.title : chat_room_user.chat_room.ad_title
     user_devices_to_receive_notification.each do |device|
-      app = APPS[device.os]
       title = chat_room_user.chat_room.system? ? "#{I18n.t("recario", locale: device.locale)} 🌀" : ad_title
 
       case device.os
       when "ios"
         notification_params = {
-          app: app,
+          app: Rpush::Client::ActiveRecord::Apnsp8::App.find_by_name(device.os), # TODO: this should be cached
           device_token: device.push_token,
           alert: "#{title}\n#{message_body}",
           sound: "default",
@@ -30,7 +24,7 @@ class SendChatMessagePushNotification
         Rpush::Client::ActiveRecord::Apnsp8::Notification.create!(notification_params)
       when "android"
         notification_params = {
-          app: app,
+          app: Rpush::Client::ActiveRecord::Gcm::App.find_by_name(device.os), # TODO: this should be cached
           registration_ids: [device.push_token],
           priority: "high",
           data: {
