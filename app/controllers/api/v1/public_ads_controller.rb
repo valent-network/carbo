5c006821f22cb217ef5adc8a9c334f872e2b88de
Viktor Vsk <me@viktorvsk.com>
2023-05-28 13:45:27 +0200

    Adjust public ads controller to work with UUIDs for native ads

--- app/controllers/api/v1/public_ads_controller.rb
+++ app/controllers/api/v1/public_ads_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class PublicAdsController < ApplicationController
       def show
-        ad = Ad.find(params[:id])
+        ad = Ad.find_by(id: params[:id]) || Ad.find_by(address: "https://recar.io/ads/#{params[:id]}")
 
         render(json: AdSerializer.new(ad).as_json.except(:friend_name_and_total, :prices, :url, :images))
       end
